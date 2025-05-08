cd8a223f93ef67bcd8e07a1575a337971de15937
Florence <florencebukola046@gmail.com>
2025-05-08 12:00:43 +0100

    Updates

---
+++ precache-manifest.4e2eefa373b329229a0f1b7154038a12.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed63f92070392e1f37e075fead592c7",
+    "url": "/MyFirst-React-Website.git/index.html"
+  },
+  {
+    "revision": "e38f3dbac2867467092e",
+    "url": "/MyFirst-React-Website.git/static/css/main.1401643b.chunk.css"
+  },
+  {
+    "revision": "debeb8dd0b4116b29ffb",
+    "url": "/MyFirst-React-Website.git/static/js/2.6becf79a.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/MyFirst-React-Website.git/static/js/2.6becf79a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38f3dbac2867467092e",
+    "url": "/MyFirst-React-Website.git/static/js/main.72016405.chunk.js"
+  },
+  {
+    "revision": "5f9a8e7bb2f5ab371fce",
+    "url": "/MyFirst-React-Website.git/static/js/runtime-main.66a1400f.js"
+  },
+  {
+    "revision": "ec6aa4954cc2597d83fd812789dda2f2",
+    "url": "/MyFirst-React-Website.git/static/media/1.ec6aa495.avif"
+  },
+  {
+    "revision": "a0325ed82c390e41a3141ea6a1ff91fc",
+    "url": "/MyFirst-React-Website.git/static/media/10.a0325ed8.avif"
+  },
+  {
+    "revision": "a91e90e9b7523c6f93eb859cb88ae543",
+    "url": "/MyFirst-React-Website.git/static/media/11.a91e90e9.jpg"
+  },
+  {
+    "revision": "aa67e6f0b78bff3a1e4d5b81dc724259",
+    "url": "/MyFirst-React-Website.git/static/media/12.aa67e6f0.avif"
+  },
+  {
+    "revision": "02723c4d47521c2a189c28f3d4b2ff56",
+    "url": "/MyFirst-React-Website.git/static/media/13.02723c4d.jpg"
+  },
+  {
+    "revision": "8904347c26416dec8a93b03048dbf001",
+    "url": "/MyFirst-React-Website.git/static/media/14.8904347c.avif"
+  },
+  {
+    "revision": "3f6cd8ca86b183a5eccf7b91485cf7fc",
+    "url": "/MyFirst-React-Website.git/static/media/15.3f6cd8ca.avif"
+  },
+  {
+    "revision": "9ecf98eb7833232105a83b8500c996e2",
+    "url": "/MyFirst-React-Website.git/static/media/16.9ecf98eb.avif"
+  },
+  {
+    "revision": "61da4352a343420ad5b551e658565149",
+    "url": "/MyFirst-React-Website.git/static/media/17.61da4352.avif"
+  },
+  {
+    "revision": "133096ef9b281d1edba13dc1623a3e2a",
+    "url": "/MyFirst-React-Website.git/static/media/18.133096ef.avif"
+  },
+  {
+    "revision": "1e1c7db816b09a63b4cecbf6d641c4e4",
+    "url": "/MyFirst-React-Website.git/static/media/19.1e1c7db8.avif"
+  },
+  {
+    "revision": "740ec2099a1dcaffd2dc023745728b67",
+    "url": "/MyFirst-React-Website.git/static/media/2.740ec209.avif"
+  },
+  {
+    "revision": "477add490e28d3601a3f06191bc9b5ff",
+    "url": "/MyFirst-React-Website.git/static/media/20.477add49.avif"
+  },
+  {
+    "revision": "0720410a8671009d9718cb81d9948f55",
+    "url": "/MyFirst-React-Website.git/static/media/21.0720410a.avif"
+  },
+  {
+    "revision": "06beed3ded9426ed8b1476c95c4a416d",
+    "url": "/MyFirst-React-Website.git/static/media/22.06beed3d.avif"
+  },
+  {
+    "revision": "7a0864a712f0d6ea590b5214a8e77a37",
+    "url": "/MyFirst-React-Website.git/static/media/24.7a0864a7.avif"
+  },
+  {
+    "revision": "7f3be53359f44bab702caa1527748627",
+    "url": "/MyFirst-React-Website.git/static/media/25.7f3be533.avif"
+  },
+  {
+    "revision": "dc12f0091285cd168b4f9b57187c2fc2",
+    "url": "/MyFirst-React-Website.git/static/media/26.dc12f009.avif"
+  },
+  {
+    "revision": "691109d2d8cf0a269f64a78113909ed2",
+    "url": "/MyFirst-React-Website.git/static/media/27.691109d2.avif"
+  },
+  {
+    "revision": "9849c6007c75d968854874a6cca48741",
+    "url": "/MyFirst-React-Website.git/static/media/28.9849c600.jpg"
+  },
+  {
+    "revision": "3f7264dae5b4b256bb2e6551f1843c2a",
+    "url": "/MyFirst-React-Website.git/static/media/29.3f7264da.jpg"
+  },
+  {
+    "revision": "1a761a3dec090fba817136cb3de6d95f",
+    "url": "/MyFirst-React-Website.git/static/media/3.1a761a3d.avif"
+  },
+  {
+    "revision": "e3192d04558a8f280c6ab431ffb790cf",
+    "url": "/MyFirst-React-Website.git/static/media/30.e3192d04.avif"
+  },
+  {
+    "revision": "ca3a0ecdd37f964787e0b030703b4a15",
+    "url": "/MyFirst-React-Website.git/static/media/31.ca3a0ecd.avif"
+  },
+  {
+    "revision": "a5470154ab37dd11430f5d5c21d0ac43",
+    "url": "/MyFirst-React-Website.git/static/media/32.a5470154.avif"
+  },
+  {
+    "revision": "eea4948f66159a828846734cab49fcfe",
+    "url": "/MyFirst-React-Website.git/static/media/33.eea4948f.avif"
+  },
+  {
+    "revision": "5df7aa0d22ca011f30e2a2e2c0293521",
+    "url": "/MyFirst-React-Website.git/static/media/4.5df7aa0d.jpg"
+  },
+  {
+    "revision": "af05d67e3f09e560d07202decdd968b8",
+    "url": "/MyFirst-React-Website.git/static/media/5.af05d67e.avif"
+  },
+  {
+    "revision": "4c3f991547933b863008a311a2166a49",
+    "url": "/MyFirst-React-Website.git/static/media/6.4c3f9915.avif"
+  },
+  {
+    "revision": "d260caf8a159118682a01f9789c4c272",
+    "url": "/MyFirst-React-Website.git/static/media/7.d260caf8.jpg"
+  },
+  {
+    "revision": "3640fc62b1e5cb23afbd8a5822d52c26",
+    "url": "/MyFirst-React-Website.git/static/media/8.3640fc62.avif"
+  },
+  {
+    "revision": "699dec989acab0d092a748fcbf190c8b",
+    "url": "/MyFirst-React-Website.git/static/media/9.699dec98.avif"
+  },
+  {
+    "revision": "59de8ced1f33d53b4c091096385cea3d",
+    "url": "/MyFirst-React-Website.git/static/media/Cart-icon.59de8ced.avif"
+  },
+  {
+    "revision": "315d88ba7fdbce732024f2f528a733a2",
+    "url": "/MyFirst-React-Website.git/static/media/Hero-img.315d88ba.png"
+  },
+  {
+    "revision": "94de06c4e3cbbddc1cbf25a32dcf3960",
+    "url": "/MyFirst-React-Website.git/static/media/facebook.94de06c4.png"
+  },
+  {
+    "revision": "91132329a4cdc9ffbbc3b5620df2a671",
+    "url": "/MyFirst-React-Website.git/static/media/instagram.91132329.png"
+  },
+  {
+    "revision": "611e82bf3036926c234ce07fa019e259",
+    "url": "/MyFirst-React-Website.git/static/media/linkedin.611e82bf.png"
+  },
+  {
+    "revision": "fec249725f56ee19f756fee989827f6b",
+    "url": "/MyFirst-React-Website.git/static/media/logo.fec24972.avif"
+  },
+  {
+    "revision": "4df8877c218b971edf46ef896fc6b607",
+    "url": "/MyFirst-React-Website.git/static/media/sparkle.4df8877c.png"
+  },
+  {
+    "revision": "db2df610e31894c01bfdcdc9053d4387",
+    "url": "/MyFirst-React-Website.git/static/media/whatsapp.db2df610.png"
+  }
+]);
